5d6b9b4e80fbefbe21ddb8cd49ee21ef9965641c
Julien <gogrinformatique@gmail.com>
2020-12-14 23:10:54 +0100

    parameter easyadmin bundle

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use Symfony\Component\Security\Core\User\UserInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Provider\AdminContextProvider;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,56 +18,34 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-//        // redirect to some CRUD controller
-//        $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
-//
-//        return $this->redirect($routeBuilder->setController(OneOfYourCrudController::class)->generateUrl());
-//
-//        // you can also redirect to different pages depending on the current user
-//        if ('jane' === $this->getUser()->getUsername()) {
-//            return $this->redirect('...');
-//        }
-//
-//        // you can also render some template to display a proper Dashboard
-//        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-//        return $this->render('some/path/my-dashboard.html.twig');
-
-
-
         return parent::index();
     }
 
-    public function configureDashboard(): Dashboard
-    {
-        return Dashboard::new()
-            // the name visible to end users
-            ->setTitle('Keep Connection.')
-            // you can include HTML contents too (e.g. to link to an image)
-            ->setTitle('<img src="../../../public/pictures/logoKeep.png"> Keep Connetion')
-
-            // the path defined in this method is passed to the Twig asset() function
-            ->setFaviconPath('favicon.svg')
-
-            // the domain used by default is 'messages'
-            ->setTranslationDomain('my-custom-domain')
-
-            // there's no need to define the "text direction" explicitly because
-            // its default value is inferred dynamically from the user locale
-            ->setTextDirection('ltr')
-
-            // set this option if you prefer the page content to span the entire
-            // browser width, instead of the default design which sets a max width
-            ->renderContentMaximized()
-
-            // set this option if you prefer the sidebar (which contains the main menu)
-            // to be displayed as a narrow column instead of the default expanded design
-            ->renderSidebarMinimized()
-            ;
-    }
-
-    public function configureMenuItems(): iterable
-    {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
-    }
+//    public function configureDashboard(): Dashboard
+//    {
+//        return Dashboard::new()
+//            // the name visible to end users
+//            ->setTitle('Keep Connection.')
+//            // you can include HTML contents too (e.g. to link to an image)
+//            ->setTitle('<img src="../../../public/pictures/logoKeep.png"> Keep Connection')
+//
+//            // the path defined in this method is passed to the Twig asset() function
+//            ->setFaviconPath('favicon.svg')
+//
+//            // the domain used by default is 'messages'
+//            ->setTranslationDomain('messages')
+//
+//            // there's no need to define the "text direction" explicitly because
+//            // its default value is inferred dynamically from the user locale
+//            ->setTextDirection('ltr')
+//
+//            // set this option if you prefer the page content to span the entire
+//            // browser width, instead of the default design which sets a max width
+//            ->renderContentMaximized()
+//
+//            // set this option if you prefer the sidebar (which contains the main menu)
+//            // to be displayed as a narrow column instead of the default expanded design
+//            ->renderSidebarMinimized()
+//            ;
+//    }
 }
